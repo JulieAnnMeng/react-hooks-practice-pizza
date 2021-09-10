56c8c0864c1a4bb2e507048398a7a6edfa7761da
Julie Meng <julieannmeng@gmail.com>
2021-09-10 09:59:21 -0400

    commit tree 08210cb8d55c1bb5d3a263e8150faed2e4656ee2

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  const [currentPizza, setCurrentPizza] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+    .then(response => response.json())
+    .then(setPizzas)
+  }, [])
+  
+  function handleEdit (pizza) {
+    setCurrentPizza(pizza);
+  }
+
+  function pizzaPost(){
+    console.log("Posted Pizza", formData)
+  }
+
+  function pizzaPatch() {
+    console.log("Patched Pizza", formData)
+  }
+
+
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm currentPizza={currentPizza} pizzaPost={pizzaPost} pizzaPatch={pizzaPatch} />
+      <PizzaList pizzas={pizzas} handleEdit={handleEdit}/>
     </>
   );
 }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PizzaForm() {
+function PizzaForm({currentPizza, pizzaPatch, pizzaPost}) {
+  const blankForm = {
+    topping: "", 
+    size: "", 
+    vegetarian: true
+  };
+  const [isEdit, setIsEdit] = useState(false)
+  const [formData, setFormData] = useState(blankForm);
+
+  if (currentPizza && isEdit === false) {
+    setIsEdit(true)
+    setFormData({
+      topping: currentPizza.topping, 
+      size: currentPizza.size,
+      vegetarian: currentPizza.vegetarian
+    });
+  }
+
+  function handleFormChange (e) {
+    setIsEdit(true)
+    const name = e.target.name;
+    let value = "";
+    if(e.target.type === "radio"){
+      value = e.target.value === "Vegetarian" ? true : false ;
+    } else {
+      value = e.target.value;
+    }
+    setFormData({...formData, [name]: value})
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData)
+    if(formData.topping === currentPizza.topping){
+      pizzaPatch(formData)
+    } else {pizzaPost(formData)}
+    setIsEdit(false);
+    setFormData(blankForm);
+  }
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
@@ -10,10 +49,17 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={formData.topping}
+            onChange={handleFormChange}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select 
+          className="form-control" 
+          name="size"
+          value={formData.size}
+          onChange={handleFormChange}
+          >
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -26,6 +72,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
+              checked={formData.vegetarian}
+              onChange={handleFormChange}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -35,6 +83,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={!formData.vegetarian}
+              onChange={handleFormChange}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
